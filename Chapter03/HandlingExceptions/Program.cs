09c858df192cf20803111125c0cf3a4eefb28479
NareshRobertson <naresh.nagandla@robertsoncollege.ca>
2022-03-02 20:08:09 -0600

    All chapters source code from packt textbook

---
+++ Chapter03/HandlingExceptions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace HandlingExceptions
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WriteLine("Before parsing");
+
+      Write("What is your age? ");
+      string input = Console.ReadLine();
+
+      try
+      {
+        int age = int.Parse(input);
+        WriteLine($"You are {age} years old.");
+      }
+      catch (Exception ex)
+      {
+        WriteLine($"{ex.GetType()} says {ex.Message}");
+      }
+      finally
+      {
+        //Console.Clear();
+      }
+
+      WriteLine("After parsing");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+# cs8dotnetcore3-master
+packt C# 8.0 and .NET Core 3.0
+=======
+# cs8dotnetcore3
+Repository for the Packt Publishing book
+## C# 8.0 and .NET Core 3.0 - Modern Cross-Platform Development, Fourth Edition
+![C# 8.0 and .NET Core 3.0 by Packt Publishing](https://github.com/markjprice/cs8dotnetcore3/blob/master/B08882_cover.jpeg)
+>>>>>>> c35078b... Upload all the chapter Solution Files
